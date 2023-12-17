3270475eb69a4dbb29f11dca4cb9348ae6a3364f
WangLab-SINH <87359159+WangLab-SINH@users.noreply.github.com>
2023-12-18 03:52:17 +0800

    Add files via upload

---
+++ HTML/src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/display.css'
+import locale from 'element-ui/lib/locale/lang/en'
+import '@/styles/element-variables.scss'
+Vue.use(ElementUI, { locale })
+Vue.use(ElementUI, {
+  size: 'small',
+})
